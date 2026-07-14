9e30e274b0c18536dfa77c543a9468e321bf541a
Avishka Edirisinghe <avishkaedi@gmail.com>
2026-07-14 21:10:53 +0530

    Update Library.java

--- Manual-Version/src/Library.java
+++ Manual-Version/src/Library.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Library {
-
     private ArrayList<Book> books;
 
     public Library() {
@@ -14,12 +13,10 @@ public class Library {
     }
 
     public void displayBooks() {
-
         if (books.isEmpty()) {
             System.out.println("No books available.");
             return;
         }
-
         System.out.println("\n========== BOOK LIST ==========");
 
         for (Book book : books) {
@@ -29,13 +26,10 @@ public class Library {
     }
 
     public void searchBook(String title) {
-
         boolean found = false;
 
         for (Book book : books) {
-
             if (book.getTitle().equalsIgnoreCase(title)) {
-
                 System.out.println("Book Found");
                 System.out.println(book);
 
@@ -52,61 +46,46 @@ public class Library {
     public void borrowBook(int id) {
 
         for (Book book : books) {
-
             if (book.getId() == id) {
-
                 if (!book.isBorrowed()) {
-
                     book.borrowBook();
                     System.out.println("Book borrowed successfully.");
 
                 } else {
-
                     System.out.println("Book is already borrowed.");
                 }
 
                 return;
             }
         }
-
         System.out.println("Book ID not found.");
     }
 
     public void returnBook(int id) {
 
         for (Book book : books) {
-
             if (book.getId() == id) {
-
                 if (book.isBorrowed()) {
 
                     book.returnBook();
                     System.out.println("Book returned successfully.");
-
                 } else {
-
                     System.out.println("Book was not borrowed.");
                 }
-
                 return;
             }
         }
-
         System.out.println("Book ID not found.");
     }
-
     public void removeBook(int id) {
 
         for (Book book : books) {
-
             if (book.getId() == id) {
-
                 books.remove(book);
                 System.out.println("Book removed successfully.");
                 return;
             }
         }
-
         System.out.println("Book ID not found.");
     }
 }
